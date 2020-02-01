4952d08083f73bc3dcdef572d69f44cdd5c8a961
Eli Lord <eli.m.lord@gmail.com>
2020-02-01 16:18:22 -0500

    Added worker allies

---
+++ RoboRepair/Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public int status = 0;
+
+    Vector3 startingPosition;
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    public void Open()
+    {
+        if (status != 1)
+        {
+            status = 1;
+            transform.position = startingPosition += transform.forward * -3;
+        }
+    }
+}
---
+++ RoboRepair/Assets/Scripts/WorkerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorkerController : MonoBehaviour
+{
+    public Material repaired;
+    double speed;
+    double reverseTimer = -1;
+    [Range(1, 3)]
+    public int behaviour = 2;
+    public Transform ButtonObjective;
+
+    NavMeshAgent agent;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        speed = 0;
+    }
+
+    public void BeginBeingBroken()
+    {
+        speed = 2;
+    }
+
+    void Update()
+    {
+        LayerMask mask = LayerMask.GetMask("Obstacles");
+
+        rb.velocity = transform.forward * (float)speed;
+
+        if (behaviour == 1)
+        {
+            if (reverseTimer > 0)
+            {
+                reverseTimer -= Time.deltaTime;
+            }
+            else if (reverseTimer != -99)
+            {
+                speed = 2;
+                reverseTimer = -99;
+            }
+
+            if (reverseTimer < 0)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 1f, mask))
+                {
+                    Debug.Log(hit.transform.gameObject.name);
+                    speed = -1f;
+                    reverseTimer = .5f;
+                }
+            }
+        }
+        if(behaviour == 2)
+        {
+            transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
+        }
+        if(behaviour == 3)
+        {
+            speed = 0;
+
+            agent.SetDestination(ButtonObjective.position);
+
+            if(Vector3.Distance(transform.position, ButtonObjective.position) < 1)
+            {
+                ButtonObjective.gameObject.GetComponent<ButtonController>().Activate();
+
+                agent.ResetPath();
+            }
+
+            
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Repair"))
+        {
+            GetComponent<Renderer>().material = repaired;
+
+            behaviour = 3;
+        }
+    }
+}
