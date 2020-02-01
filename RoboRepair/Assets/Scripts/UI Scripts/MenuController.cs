63201fb2b77c65f3192b88ed588b56c77704ed59
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-02-01 00:34:53 -0500

    Began implementing UI

---
+++ RoboRepair/Assets/Scripts/UI Scripts/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    public RectTransform codingPanel;
+
+    public RectTransform inventoryContent;
+
+    public RectTransform instructionContent;
+
+    public TextMeshProUGUI toggleText; // Delete when sprites for toggle are created
+
+    private bool showing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    public void ToggleMenu()
+    {
+        if (showing)
+        {
+            codingPanel.anchoredPosition = new Vector2(384, 0);
+            toggleText.text = "<"; // Delete when sprites for toggle are created
+            showing = false;
+        }
+        else
+        {
+            codingPanel.anchoredPosition = new Vector2(-384, 0);
+            toggleText.text = ">"; // Delete when sprites for toggle are created
+            showing = true;
+        }
+    }
+}
