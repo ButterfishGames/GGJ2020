a2e66d8b70ed46a4d177bd98a014426cd29c9fa4
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-02-01 20:13:49 -0500

    Created Menu (needs scene links still)

---
+++ RoboRepair/Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public static MainMenuController singleton;
+
+    public int levelOneBuildIndex;
+
+    public int creditsBuildIndex;
+
+    public Transform canvas;
+
+    public Transform inventoryContent;
+
+    public MenuSlotController slot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void RunScript()
+    {
+        switch (slot.block.GetComponent<MenuBlockController>().blockType)
+        {
+            case MenuBlockController.MenuBlockType.play:
+                Play();
+                break;
+
+            case MenuBlockController.MenuBlockType.credits:
+                RollCredits();
+                break;
+
+            case MenuBlockController.MenuBlockType.quit:
+                Application.Quit();
+                break;
+
+            default:
+                Debug.Log("ERROR: Invalid MenuBlockType");
+                break;
+        }
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadScene(levelOneBuildIndex);
+    }
+
+    private void RollCredits()
+    {
+        SceneManager.LoadScene(creditsBuildIndex);
+    }
+}
---
+++ RoboRepair/Assets/Scripts/WackEController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WackEController : MonoBehaviour
+{
+    public string[] lines;
+
+    public void Speak()
+    {
+        DialogueManager.singleton.StartDialogue(lines);
+    }
+}
