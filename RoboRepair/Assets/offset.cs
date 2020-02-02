a8437bbb5eb31912528bc38e324cbcd7b4fff072
Eli Lord <eli.m.lord@gmail.com>
2020-02-02 14:15:20 -0500

    added arm animations

---
+++ RoboRepair/Assets/offset.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offset : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Animator>().SetFloat("offset", Random.Range(.85f, 1.15f));
+    }
+}
